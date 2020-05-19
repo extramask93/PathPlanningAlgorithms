ba8fcbfa50321c4ca9af867461725f1d4be410ae
extramask93 <jozwiak.damian02@gmail.com>
2020-05-19 15:58:56 +0200

    added travis and conan

---
+++ cmake/Conan.cmake
@@ -0,0 +1,28 @@
+macro(run_conan)
+    # Download automatically, you can also just copy the conan.cmake file
+    if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+        message(
+                STATUS
+                "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+        file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake")
+    endif()
+
+    include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+    conan_add_remote(NAME bincrafters URL
+            https://api.bintray.com/conan/bincrafters/public-conan)
+
+    conan_cmake_run(
+            REQUIRES
+            ${CONAN_EXTRA_REQUIRES}
+            catch2/2.12.1
+            boost/1.73.0
+            eigen/3.3.7
+            OPTIONS
+            ${CONAN_EXTRA_OPTIONS}
+            BASIC_SETUP
+            CMAKE_TARGETS # individual targets to link to
+            BUILD
+            missing)
+endmacro()
