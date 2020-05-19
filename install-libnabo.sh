654d343e690012d388a1f3e4b129b9baa2645b15
Damian Jóźwiak <jozwiak.damian02@gmail.com>
2020-05-19 18:51:51 +0200

    Update install-libnabo.sh

--- install-libnabo.sh
+++ install-libnabo.sh
@@ -4,5 +4,5 @@ git clone --depth 1 https://github.com/ethz-asl/libnabo.git libnabo
 cd libnabo
 mkdir build
 cd build
-cmake .. && cmake --build . --config Release && sudo cmake --install
+cmake .. && make && sudo make install
 
